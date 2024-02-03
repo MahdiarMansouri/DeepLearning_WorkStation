aeddf88079d388e53bc5c61d95124e8f57862d37
MahdiarMansouri <mahdiarm60@gmail.com>
2024-02-03 19:15:08 +0330

    initial commit

--- models.py
+++ models.py
@@ -1,19 +1,18 @@
-import torch
-import torch.nn as nn
 import torchvision
 
 
-class Model(nn.Module):
+class Model():
     def __init__(self, model_name):
-        # super().__init__(Model)
         self.model_name = model_name
 
     def create_model(self):
         model = torchvision.models.get_model(self.model_name)
         return model
 
+
 M = Model('resnet50')
 model = M.create_model()
-# print(param for param in model.parameters())
+print(model.fc.out_features)
+
 for child in model.children():
     print(child)
